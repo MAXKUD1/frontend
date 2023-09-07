dbbd77b49795870cd7da7df7b32ecd187c341557
wijitsinp <Vijisin01@gmail.com>
2023-09-07 14:13:36 +0700

    456

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -10,7 +10,7 @@ export const authOptions = {
             password: { label: "Password", type: "password" }
           },
           async authorize(credentials, req) {
-            const res = await fetch("frontend-eight-gray-66.vercel.app/api/login", {
+            const res = await fetch("http://localhost:3000/api/login", {
               method: 'POST',
               body: JSON.stringify(credentials),
               headers: { "Content-Type": "application/json" }

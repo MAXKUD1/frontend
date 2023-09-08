bf1ed8ce71d8ecb804bb1ef6574478a0832fd173
wijitsinp <Vijisin01@gmail.com>
2023-09-08 11:00:46 +0700

    1

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -69,7 +69,7 @@ export default function Component({ posts }) {
                   </tr>
                 </thead>
                 <tbody>
-                  {posts.users.map((post) => (
+                  {posts.user.map((post) => (
                     <tr key={post.id}>
                       <td className="text-center">{post.id}</td>
                       <td className= "text-center">{post.studentid}</td>
